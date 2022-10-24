c6a515e1cccd3af67c9805b0b597775809693a1b
DANIEL2DAM <dmarrom577@g.educaand.es>
2022-10-24 12:41:26 +0200

    insti

---
+++ src/main/java/com/example/calculadoracss/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.calculadoracss;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+
+    @FXML
+    private Button verde;
+    @FXML
+    private Button verde2;
+    @FXML
+    private Button verde3;
+    @FXML
+    private Button verde4;
+}
